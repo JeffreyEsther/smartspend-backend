d3328d005bc80ae7aced024ea4ff38f8a9437702
jnboakye <jaeyblaq2131@gmail.com>
2025-04-18 22:13:39 +0000

    wishlist validators

--- validators/wishlistValidator.js
+++ validators/wishlistValidator.js
@@ -4,6 +4,5 @@ export const wishlistSchema = Joi.object({
     item: Joi.string().required(),
     targetAmount: Joi.number().required(),
     savedAmount: Joi.number().min(0).default(0),
-    priority: Joi.string().valid('low', 'medium', 'high').default('medium'),
-    type: Joi.string().valid('need', 'want').required(),
+    priority: Joi.string().valid('low', 'medium', 'high').default('medium')
 });
